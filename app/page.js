fa6a7ef75cf049f7aec01d2502c9314dc6e95b49
zHoN-dev <jaysonborja0108@gmail.com>
2026-09-22 11:59:11 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,35 +3,35 @@
 import { useState } from "react";
 
 const FIRST_NAMES = {
-  A: ["Andrea","Angela","Anne","Alyssa","Aira"],
-  B: ["Bianca","Bea","Bernadette","Brianna","Bella"],
-  C: ["Camille","Carla","Christine","Chloe","Clarisse"],
-  D: ["Danielle","Diana","Denise","Daphne","Daniella"],
-  E: ["Ella","Elaine","Erika","Eunice","Elise"],
-  F: ["Faith","Fiona","Frances","Faye","Felicia"],
-  G: ["Gabrielle","Giselle","Grace","Gwen","Gia"],
-  H: ["Hannah","Hazel","Heidi","Hope","Holly"],
-  I: ["Isabel","Irene","Ivy","Isabelle","Imani"],
-  J: ["Jasmine","Janelle","Joanna","Julia","Jessa"],
-  K: ["Katrina","Kyla","Kristine","Kara","Kim"],
-  L: ["Leah","Lara","Lianne","Liza","Luna"],
-  M: ["Maria","Mikaela","Megan","Michelle","Mae"],
-  N: ["Nicole","Nina","Natalie","Naomi","Nadine"],
-  O: ["Olivia","Odessa","Ophelia","Olive","Orla"],
-  P: ["Patricia","Paula","Phoebe","Pia","Priscilla"],
-  Q: ["Queenie","Quinn","Quiana","Querida","Quella"],
-  R: ["Rachel","Rina","Rhea","Rebecca","Rochelle"],
-  S: ["Sophia","Samantha","Sarah","Selena","Stephanie"],
-  T: ["Theresa","Tanya","Tricia","Tessa","Tina"],
-  U: ["Uma","Ursula","Unity","Una","Ulani"],
-  V: ["Vanessa","Veronica","Valerie","Vera","Violet"],
-  W: ["Wendy","Willa","Whitney","Willow","Winnie"],
-  X: ["Xena","Xandra","Xyla","Xiomara","Xenia"],
-  Y: ["Yasmin","Yvette","Yvonne","Yana","Yara"],
-  Z: ["Zara","Zoe","Zinnia","Zelda","Zia"]
+  A: ["Aaliyah", "Abigail", "Ada", "Adaline", "Adalyn", "Addison", "Adela", "Adele", "Adriana", "Adrianna", "Aileen", "Aimee", "Alana", "Alaina", "Alana", "Alessandra", "Alexa", "Alexandra", "Alice", "Alicia", "Alina"],
+  B: ["Bailey", "Barbara", "Beatrice", "Becca", "Bella", "Belle", "Bernice", "Beth", "Bethany", "Betsy", "Bianca", "Billie", "Blair", "Blake", "Blythe", "Bonnie", "Brenda", "Briana", "Brianna", "Bridget", "Brittany", "Brooke", "Brooklyn"],
+  C: ["Caitlin", "Callie", "Camila", "Camille", "Cara", "Carla", "Carmen", "Carolina", "Caroline", "Carolyn", "Casey", "Cassandra", "Cassie", "Catherine", "Cecilia", "Celeste", "Celine", "Chanel", "Charlotte", "Chelsea", "Chloe", "Christina", "Christine", "Claire", "Clara", "Clarissa"],
+  D: ["Dahlia", "Daisy", "Dakota", "Daniela", "Danielle", "Daphne", "Darla", "Darlene", "Davina", "Dawn", "Deanna", "Deborah", "Delia", "Delilah", "Denise", "Desiree", "Destiny", "Diana", "Diane", "Dianne", "Dora", "Dorothy"],
+  E: ["Eden", "Edith", "Edna", "Eileen", "Elaine", "Eleanor", "Elena", "Eliana", "Elisa", "Elise", "Eliza", "Elizabeth", "Ella", "Ellen", "Ellie", "Eloise", "Elsa", "Elsie", "Emilia", "Emily", "Emma", "Erica", "Erika", "Erin", "Esme", "Esther", "Eva", "Eve", "Evelyn"],
+  F: ["Faith", "Fallon", "Farrah", "Fatima", "Faye", "Felicia", "Felicity", "Fern", "Fiona", "Flora", "Florence", "Frances", "Francesca", "Frankie", "Freya", "Frida", "Gabriella", "Giselle"],
+  G: ["Gabriela", "Gabriella", "Gail", "Gemma", "Genesis", "Georgia", "Georgina", "Geraldine", "Gia", "Gianna", "Gigi", "Gina", "Ginger", "Giovanna", "Giselle", "Gloria", "Grace", "Gracie", "Greta", "Gretchen", "Gwen", "Gwendolyn", "Guadalupe"],
+  H: ["Hadley", "Hailey", "Haley", "Hallie", "Hannah", "Hanna", "Harmony", "Harper", "Harriet", "Hattie", "Hazel", "Heather", "Heidi", "Helen", "Helena", "Holly", "Hope", "Hunter", "Hyacinth", "Hayley", "Henrietta", "Honor", "Hilda", "Hillary"],
+  I: ["Ida", "Iliana", "Iliana", "Imelda", "Imani", "Imogen", "India", "Indigo", "Ingrid", "Irene", "Irina", "Iris", "Isabel", "Isabella", "Isabelle", "Isla", "Isobel", "Ivy", "Ivana", "Ivette", "Inez"],
+  J: ["Jackie", "Jade", "Jada", "Janelle", "Jane", "Janet", "Janice", "Jasmine", "Jayla", "Jean", "Jenna", "Jennifer", "Jenny", "Jessica", "Jessie", "Jill", "Joanna", "Jocelyn", "Josephine", "Josie", "Joy", "Joyce", "Judith", "Julia", "Juliana", "Juliet", "Julie", "June"],
+  K: ["Kaitlyn", "Kara", "Karen", "Karina", "Karla", "Kassandra", "Kate", "Katelyn", "Katherine", "Kathleen", "Kathryn", "Katie", "Kayla", "Keira", "Kelly", "Kendall", "Kennedy", "Kiera", "Kim", "Kimberly", "Kirsten", "Krista", "Kristen", "Kristina", "Kristine", "Kylie", "Kyla"],
+  L: ["Lacey", "Laila", "Lana", "Lara", "Laura", "Lauren", "Layla", "Leah", "Leanne", "Leslie", "Leticia", "Lia", "Liana", "Lillian", "Lily", "Liliana", "Lina", "Linda", "Lindsey", "Lisa", "Lizbeth", "Lola", "London", "Lorelei", "Lorraine", "Lucia", "Lucille", "Lucy", "Luna", "Lydia"],
+  M: ["Mabel", "Mackenzie", "Macy", "Madeline", "Madison", "Mae", "Maggie", "Maia", "Maisie", "Makayla", "Mallory", "Mara", "Maria", "Mariah", "Mariana", "Marianne", "Marie", "Marissa", "Marisol", "Marley", "Martha", "Mary", "Matilda", "Maya", "Megan", "Melody", "Melanie", "Melissa", "Mia", "Michaela", "Michelle", "Mila", "Millie", "Miranda", "Molly", "Monica", "Monique", "Morgan", "Mya"],
+  N: ["Nadia", "Nadine", "Nancy", "Naomi", "Natalia", "Natalie", "Natasha", "Naya", "Nevaeh", "Nicole", "Nicola", "Nina", "Noelle", "Nora", "Norah", "Norma", "Nova", "Nydia", "Nyla", "Nia", "Nellie", "Nerissa", "Nerine"],
+  O: ["Oakley", "Octavia", "Odessa", "Odette", "Olga", "Olive", "Olivia", "Opal", "Ophelia", "Ora", "Oriana", "Orla", "Ornella", "Oona", "Odelia", "Olympia", "Ocean", "Oksana", "Onyx", "Opaline", "Orielle"],
+  P: ["Paige", "Paisley", "Paloma", "Pamela", "Patricia", "Paula", "Paulina", "Pearl", "Penelope", "Penny", "Phoebe", "Piper", "Pippa", "Poppy", "Presley", "Priya", "Priscilla", "Prudence", "Paige", "Portia", "Petra", "Pilar", "Pia", "Polly", "Posy"],
+  Q: ["Queenie", "Quinn", "Quiana", "Quilla", "Querida", "Quetzalli", "Qiana", "Qadira", "Qamar", "Qiana", "Quinley", "Quinlan", "Quinta", "Quintessa", "Quella", "Quenby", "Quirina", "Quita", "Qiana", "Quorra", "Qiana"],
+  R: ["Rachel", "Rachael", "Raegan", "Raelynn", "Rebecca", "Rebekah", "Regina", "Reina", "Renee", "Rhea", "Rhoda", "Riley", "Rita", "River", "Robin", "Robyn", "Rochelle", "Rosa", "Rosalie", "Rosalind", "Rose", "Rosemary", "Rosie", "Rowan", "Roxanne", "Ruby", "Ruth"],
+  S: ["Sabrina", "Sadie", "Sally", "Samantha", "Samara", "Sandra", "Sara", "Sarah", "Savannah", "Scarlett", "Selena", "Serena", "Serenity", "Shania", "Shannon", "Sharon", "Shayla", "Sheila", "Shelby", "Sierra", "Simone", "Skylar", "Sofia", "Sophia", "Sonia", "Sonya", "Stella", "Stephanie", "Summer", "Susan", "Susanna", "Sydney", "Sylvia"],
+  T: ["Tabitha", "Talia", "Tamara", "Tania", "Tanya", "Tara", "Tatiana", "Tatum", "Taylor", "Teresa", "Theresa", "Tessa", "Tia", "Tiffany", "Tiana", "Tiara", "Tori", "Tricia", "Trinity", "Trisha", "Trudy", "Teresa", "Thea", "Thalia"],
+  U: ["Ula", "Ulani", "Una", "Unity", "Ursula", "Ursa", "Uriah", "Ulyssa", "Uma", "Umbria", "Undine", "Usha", "Usagi", "Udele", "Ulla", "Ulrica", "Ulrika", "Umeko", "Umi", "Usha", "Uriella", "Uriel"],
+  V: ["Valentina", "Valerie", "Vanessa", "Vera", "Veronica", "Victoria", "Vienna", "Violeta", "Violet", "Viola", "Vivian", "Viviana", "Vivienne", "Virginia", "Viridiana", "Vita", "Vittoria", "Vivienne", "Vada", "Valeria", "Vania", "Vanna", "Velma", "Venus", "Verity"],
+  W: ["Wanda", "Wendy", "Whitney", "Willa", "Willow", "Wilma", "Winifred", "Winnie", "Winter", "Wren", "Wrenley", "Wynne", "Wynona", "Waverly", "Wallace", "Wendy", "Westlyn", "Whitley", "Wilhelmina", "Winona", "Winslow", "Wednesday", "Wynter", "Wisteria"],
+  X: ["Xandra", "Xanthe", "Xara", "Xena", "Xenia", "Xiomara", "Ximena", "Xyla", "Xylia", "Xylina", "Xylah", "Xylona", "Xaria", "Xaviera", "Xavia", "Xitlali", "Xochitl", "Xoey", "Xuri", "Xena", "Xyla"],
+  Y: ["Yadira", "Yana", "Yara", "Yasmin", "Yasmine", "Yvette", "Yvonne", "Yulia", "Yuliana", "Yuna", "Yvette", "Yara", "Yolanda", "Yaritza", "Yasmina", "Yessenia", "Yelena", "Yohana", "Yvette", "Yuki", "Yumi", "Yuna"],
+  Z: ["Zahra", "Zaida", "Zaira", "Zara", "Zaria", "Zariah", "Zayla", "Zelda", "Zelina", "Zenaida", "Zinnia", "Zoe", "Zoey", "Zola", "Zora", "Zoya", "Zuleika", "Zuri", "Zariah", "Zelia", "Zenobia", "Zena"]
 };
 
-const SURNAMES = ["Santos","Reyes","Garcia","Cruz","Mendoza","Dela Cruz","Navarro","Castillo","Ramos","Aquino","Flores","Torres","Villanueva","Rivera","Bautista"];
+const SURNAMES = ["Santos", "Reyes", "Garcia", "Cruz", "Mendoza", "Navarro", "Castillo", "Ramos", "Aquino", "Flores", "Torres", "Villanueva", "Rivera", "Bautista", "Gonzales", "Gonzalez", "Dela", "Cruz", "Delacruz", "Diaz", "Fernandez", "Hernandez", "Lopez", "Martinez", "Morales", "Perez", "Ramirez", "Rodriguez", "Sanchez", "Santiago", "Soriano", "Mercado", "Manalo", "de", "Leon", "de", "Guzman", "de", "Vera", "de", "Jesus", "Evangelista", "Valdez", "Valencia", "Vargas", "Velasco", "Ventura", "Villarama", "Villarica", "Villanueva", "Villegas", "Vicente", "Velez", "Vera", "Vergara", "Yao", "Yap", "Yu", "Zamora", "Zaragoza", "Abad", "Aguilar", "Alcaraz", "Alcantara", "Alejo", "Alfonso", "Alonzo", "Alvarez", "Amador", "Amante", "Andres", "Angeles", "Angulo", "Aquino", "Arce", "Arcilla", "Arellano", "Arias", "Arriola", "Atienza", "Austria", "Avila", "Bacani", "Badillo", "Balagtas", "Balboa", "Ballesteros", "Baltazar", "Banaag", "Bandala", "Barrios", "Bartolome", "Bautista", "Bayani", "Beltran", "Bernardo", "Besa", "Biscocho", "Bonifacio", "Borja", "Briones", "Bueno", "Caballero", "Cabrera", "Calma", "Caluag", "Camacho", "Campos", "Canlas", "Cañete", "Caparas", "Capili", "Carandang", "Cariaga", "Carpio", "Casimiro", "Castañeda", "Castro", "Catacutan", "Cayabyab", "Cayanan", "Celis", "Chua", "Co", "Coloma", "Concepcion", "Contreras", "Corpuz", "Cortez", "Cordero", "Coronel", "Correa", "Cuenca", "Cunanan", "Dacanay", "Dagdag", "Dalisay", "Damasco", "David", "Dayao", "de", "Castro", "de", "la", "Peña", "de", "los", "Santos", "de", "Mesa", "de", "Ocampo", "de", "los", "Reyes", "de", "Leon", "del", "Mundo", "del", "Rosario", "Domingo", "Dominguez", "Dorado", "Duran", "Dizon", "Ecleo", "Esguerra", "Espiritu", "Estrella", "Fabian", "Fajardo", "Fajardo", "Falguera", "Fermin", "Ferrer", "Francisco", "Franco", "Fuentes", "Galang", "Gallardo", "Garcia", "Garces", "Gatdula", "Gatchalian", "Generoso", "Geronimo", "Go", "Gomez", "Gonzales", "Guevarra", "Gutierrez", "Guzman", "Hidalgo", "Hilario", "Ignacio", "Ilagan", "Infante", "Isidro", "Javier", "Jimenez", "Jose", "Juarez", "Kalaw", "Kasilag", "Katigbak", "Kintanar", "Labasan", "Lacson", "Lagman", "Lagrimas", "Lantican", "Laurel", "Legaspi", "Lim", "Linares", "Lising", "Loyola", "Macapagal", "Macaraig", "Macasieb", "Macatol", "Manahan", "Mangubat", "Manlapaz", "Mariano", "Marquez", "Marcelo", "Marinas", "Marzan", "Masangkay", "Mateo", "Matias", "Mauricio", "Medina", "Medrano", "Mejia", "Mendiola", "Meneses", "Mercado", "Mijares", "Miranda", "Molina", "Montero", "Montemayor", "Montecillo", "Montilla", "Morales", "Natividad", "Nazario", "Nicolas", "Nieves", "Nolasco", "Nunez", "Ocampo", "Ochoa", "Ong", "Ordonez", "Osias", "Pabalan", "Pacheco", "Padilla", "Paguia", "Palanca", "Palomares", "Pangilinan", "Panganiban", "Panlilio", "Paras", "Pascual", "Pastor", "Patricio", "Pelaez", "Pelayo", "Pena", "Peralta", "Perez", "Pimentel", "Pineda", "Ponce", "Prado", "Prieto", "Quijano", "Quinto", "Ramirez", "Ramos", "Real", "Reyes", "Ricarte", "Rivera", "Robles", "Roces", "Roman", "Romero", "Rosales", "Roxas", "Rubio", "Rueda", "Ruiz", "Salazar", "Salcedo", "Samson", "San", "Jose", "Sandoval", "Santiago", "Santos", "Sebastian", "Sevilla", "Sison", "Solis", "Soriano", "Suarez", "Sumulong", "Tabora", "Tan", "Tanchoco", "Tanyag", "Tañada", "Tiu", "Tolentino", "Trinidad", "Tuazon", "Uy", "Valdez", "Valenzuela", "Valerio", "Velasco", "Ventura", "Vergara", "Vicente", "Villafuerte", "Villalon", "Villanueva", "Villar", "Villarreal", "Villegas", "Yabut", "Yambao", "Yanga", "Ylagan", "Yulo", "Zafra", "Zamora", "Zapanta", "Zarate", "Zulueta", "Zuniga"];
 
 const LOCATION_DATA = {
   "Antipolo": {
@@ -49,6 +49,9 @@ const LOCATION_DATA = {
 };
 
 function pick(arr) { return arr[Math.floor(Math.random() * arr.length)]; }
+
+// Keep generated full names unique during the current browser session.
+const USED_FULL_NAMES = new Set();
 
 function birthdayForAge(age) {
   const today = new Date();
@@ -77,12 +80,26 @@ function generateProfiles({ initial, location, count, age, password }) {
   const key = (initial || "M").trim().charAt(0).toUpperCase();
   const names = FIRST_NAMES[key] || FIRST_NAMES.M;
   const loc = LOCATION_DATA[location] || LOCATION_DATA.Antipolo;
+  const totalCombinations = names.length * SURNAMES.length;
+
   return Array.from({ length: count }, (_, i) => {
-    const first = pick(names);
-    const surname = pick(SURNAMES);
+    // Prevent repeats across Generate clicks while there are unused combinations.
+    if (USED_FULL_NAMES.size >= totalCombinations) USED_FULL_NAMES.clear();
+
+    let first;
+    let surname;
+    let fullName;
+    do {
+      first = pick(names);
+      surname = pick(SURNAMES);
+      fullName = `${first} ${surname}`;
+    } while (USED_FULL_NAMES.has(fullName));
+
+    USED_FULL_NAMES.add(fullName);
+
     return {
       id: crypto.randomUUID(),
-      name: `${first} ${surname}`,
+      name: fullName,
       gender: "Female",
       age,
       birthday: birthdayForAge(age),
